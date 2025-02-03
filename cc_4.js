d33d83b2b92b3154514813e6ec49d5be04702fa5
Karim <konanfoi@gmail.com>
2025-02-02 22:22:15 -0500

    Task 2 - Sales Report

--- cc_4.js
+++ cc_4.js
@@ -8,3 +8,11 @@ if (purchaseAmount > 100) {
 }    
 let finalAmount = purchaseAmount - discount;
 console.log(`Final amount after discount: $${finalAmount}`);
+
+//Task 2 - Sales Report
+let sales = [500, 600, 700, 800, 900]; //Declare an array
+let totalSales = 0; //Initialize count variable
+for (let i=0; i< sales.length; i++) { //Use a  for loop to calculate total sales
+totalSales += sales[i];// Add each sale
+}
+console.log(`Total Sales: $${totalSales}`); //Log
